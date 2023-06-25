7f2dc7cdeb3e26d1ba8e541a2c0f545dc91bbdd1
Babajide Williams <babanila@yahoo.com>
2023-06-25 10:46:18 +0200

    Enable different pages on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:first_app/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:first_app/home_page.dart';
+import 'package:first_app/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,13 +28,17 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int currentPage = 0;
+  List<Widget> pages = [
+    const HomePage(),
+    const ProfilePage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter'),
       ),
-      body: const HomePage(),
+      body: pages[currentPage],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint('Flutter floating action button is pressed');
