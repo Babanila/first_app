fb6681610c9202e5986933752c1a2155a71fb1d9
Babajide Williams <babanila@yahoo.com>
2023-06-25 10:31:24 +0200

    Add functionality to LearnFlutterPage

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -8,6 +8,8 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool? isCheckbox = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +21,106 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           onPressed: () {
             Navigator.of(context).pop();
           },
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('Action Info Icon');
+            },
+            icon: const Icon(Icons.info_outline),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Image.asset(
+              'images/albert_einstein_talents.jpeg',
+              width: double.infinity,
+              fit: BoxFit.fill,
+            ),
+            const SizedBox(height: 10),
+            const Divider(color: Colors.green),
+            Container(
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'This is a text widget',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwitch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () {
+                debugPrint('You click Elevated button');
+              },
+              child: const Text('Elevated Button'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('You click Outlined button');
+              },
+              child: const Text('Outlined Button'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint('You click Text button');
+              },
+              child: const Text('Text Button'),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('This is the row');
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                  Text('Row Usage'),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+              value: isSwitch,
+              onChanged: (bool newBool) {
+                setState(() {
+                  isSwitch = newBool;
+                });
+              },
+            ),
+            Checkbox(
+              value: isCheckbox,
+              onChanged: (bool? newBool) {
+                setState(() {
+                  isCheckbox = newBool;
+                });
+              },
+            ),
+            Image.network('https://wallpaperaccess.com/full/1909531.jpg'),
+            Image.asset(
+              'images/nature_six.jpeg',
+              width: double.infinity,
+              fit: BoxFit.fill,
+            ),
+            Image.network(
+              'https://c4.wallpaperflare.com/wallpaper/154/813/296/albert-einstein-quote-wallpaper-preview.jpg',
+            ),
+          ],
         ),
       ),
     );
